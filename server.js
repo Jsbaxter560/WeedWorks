0f4f9f00b2c4cdd79bef9c5fc48d186ead95475a
John Baxter <juancho420@ymail.com>
2021-05-25 13:41:31 -0700

    installed mongoose, started building server, and built userSchema in models.js

--- server.js
+++ server.js
@@ -10,8 +10,32 @@ app.use(express.json());
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-
 // Define API routes here
+
+app.get("api/sativa/strains", function (req, res) {
+  //database query to get all sativa strains
+  res.json([])
+});
+
+appp.get("api/indica/strains", function (req, res) {
+  //database query to get all indica strains
+  res.json([])
+});
+
+app.get("api/hybrid/strains", function (req, res) {
+  //database query to get all hybrid strains
+  res.json([])
+});
+
+app.get("api/strains/:id", function (req, res) {
+  //database query to get strain by id?
+  res.json([])
+});
+
+app.post("api/pages/profilepage", function (req, res) {
+  //database querry to update favorites list
+  res.json([])
+});
 
 // Send every other request to the React app
 // Define any API routes before this runs
